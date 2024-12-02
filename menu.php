c63bf919b6721f6f8e1e740a4d870a10799c94ad
StefanoRC <163069852+StefanoRC@users.noreply.github.com>
2024-12-02 17:49:23 -0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,26 @@
+<!-- Menú de navegación -->
+<nav class="navbar navbar-expand-lg bg-dark" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+        <a class="navbar-brand text-warning fs-1" href="index.php">Olimpo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active text-warning fs-4" aria-current="page" href="peliculas.php">Tabla De Jugadores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-warning fs-4" aria-current="page" href="contacto.php">Fechas Proximas</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" name="buscar" value="" type="search" placeholder="Nombre Del Jugador"
+                    aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+        </div>
+    </div>
+</nav>
+<!-- Fin Menú de navegación -->
